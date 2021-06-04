215bccd54a91efd6f24a8c8209310d7f177cf65b
gywp <gywp1217@gmail.com>
2021-06-04 16:38:12 +0900

    수정

--- Assets/game.cs
+++ Assets/game.cs
@@ -6,14 +6,14 @@ using UnityEngine.UI;
 
 public class game : MonoBehaviour
 {
-    public player1 player;
+   
     public TextMesh highscore;
-    public TextMesh endscore;
+   
     // Start is called before the first frame update
     void Start()
     {
         highscore.text = PlayerPrefs.GetFloat("BestScore").ToString("N0");
-        endscore.text = player.topScore.ToString();
+       
     }
 
     // Update is called once per frame
